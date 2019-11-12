81afe6d2ecfba405bddd501224524929e6fbbcd7
gui-hub <guilherme.est9@gmail.com>
2019-11-12 20:39:21 -0300

    comprehension

---
+++ curso-python/comprehension/comprehension_v1.py
@@ -0,0 +1,10 @@
+# [ expressão for item in list if condicional]
+
+dobros = [i * 2 for i in range(10)]
+print(dobros)
+
+# lista_vazia = []
+# for i in range(10):
+#     #    i *= 2
+#     lista_vazia.append(i * 2)
+# print(lista_vazia)
---
+++ curso-python/comprehension/comprehension_v2.py
@@ -0,0 +1,10 @@
+# [ expressão for item in list if condicional]
+
+dobro_dos_pares = [i * 2 for i in range(10) if i % 2 == 0]
+print(dobro_dos_pares)
+
+# lista_vazia = []
+# for i in range(10):
+#     if i % 2 == 0:
+#         lista_vazia.append(i * 2)
+# print(lista_vazia)
